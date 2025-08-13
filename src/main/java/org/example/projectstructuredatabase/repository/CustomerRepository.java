d3205ebca35539140e1aee425bf5203dab4a84a1
IsmayilRahimov <rehimovismayil650@gmail.com>
2025-08-13 19:39:46 +0400

    //database

--- src/main/java/org/example/projectstructuredatabase/repository/CustomerRepository.java
+++ src/main/java/org/example/projectstructuredatabase/repository/CustomerRepository.java
@@ -2,6 +2,9 @@ package org.example.projectstructuredatabase.repository;
 
 import org.example.projectstructuredatabase.dto.Customer;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
 
 public interface CustomerRepository extends MongoRepository<Customer, String> {
 
